bfc5f559cbe707b8bf3f3fcb952c5a2c7bc68cdb
htbmixbox <mizdebsk@gmail.com>
2024-04-25 04:26:46 -0700

    1.9

--- CSLab/Labs/1.9/P1_9.cs
+++ CSLab/Labs/1.9/P1_9.cs
@@ -11,10 +11,43 @@ internal class P1_9
 
     public void main(String[] args)
     {
-        //Napisz w edytorze tekstowym program w C# wypisujący tekst podany jako parametr uruchomienia. Skompiluj z linii komend i uruchom.
-        Console.WriteLine("Program 1.1:\nEnter text:");
-        string intput = Console.ReadLine();
-        Console.WriteLine("Entered: "+intput);
+            //Napisz program, który dodaje n kolejnych liczb naturalnych  przy użyciu pętli. 
+            //Zliczanie ma trwać dotąd dopóki suma nie przekroczy określonej granicy. 
+            //Wówczas program ma skończyć działanie bez doliczenia tego składnika, który spowodował przekroczenie ustalonego progu.
+            //Program powinien podać, ile składników policzył i ile wynosi suma.
+            //Napisz trzykrotnie ten program -przy użyciu pętli for, while, do while.
+
+            int numberOfNumbers = 10;
+            int maxValue = 100;
+            int currentValue = 0;
+            for(int i = 0; i < maxValue; i++)
+            {
+                int newValue = currentValue + i;
+                if(maxValue<newValue)
+                {
+                    break;
+                }
+                currentValue = newValue;
+            }
+            Console.WriteLine("for:"+currentValue);
+            currentValue = 0;
+            int step= 0;
+            while(currentValue <= maxValue) { 
+            currentValue = currentValue + step;
+                step++;
+            }
+            Console.WriteLine("do:" + (currentValue-step+1));
+            currentValue= 0;
+            step = 0;
+            do { currentValue += step;
+                step++;
+            }
+            
+            while(currentValue < maxValue);
+            Console.WriteLine("do while:"+(currentValue-step+1));
+
+
+
+        }
     }
-}
 }
