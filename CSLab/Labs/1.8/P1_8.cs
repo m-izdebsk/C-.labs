5f5c15ec5d5bc6b3e6049027ea1351321928d285
htbmixbox <mizdebsk@gmail.com>
2024-04-25 04:10:16 -0700

    1.8

--- CSLab/Labs/1.8/P1_8.cs
+++ CSLab/Labs/1.8/P1_8.cs
@@ -11,10 +11,36 @@ internal class P1_8
 
     public void main(String[] args)
     {
-        //Napisz w edytorze tekstowym program w C# wypisujący tekst podany jako parametr uruchomienia. Skompiluj z linii komend i uruchom.
-        Console.WriteLine("Program 1.1:\nEnter text:");
-        string intput = Console.ReadLine();
-        Console.WriteLine("Entered: "+intput);
+            //Napisz program, który wyświetla 10 kolejnych liczb (np. od 0 do 9) przy użyciu pętli. 
+            //            Napisz 3 wersje tego programu z użyciem pętli for, while oraz do while.
+            int loopNumber = 10;
+            int startFrom = 0;
+            Console.WriteLine("Let's check loops:");
+            {
+                Console.WriteLine("for:");
+                for (int i = startFrom; i < loopNumber; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                int counter = startFrom;
+                Console.WriteLine("while:");
+                while (counter < loopNumber) {
+                    Console.WriteLine(counter);
+                    counter++;
+                }
+                counter = startFrom;
+                Console.WriteLine(" do while:");
+                do { 
+                    Console.WriteLine(counter);
+                    counter++;
+                } while (counter < loopNumber);
+                    
+
+
+            }
+            
+        
+        
     }
 }
 }
