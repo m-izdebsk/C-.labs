1bbb6c871f3c7825b534f43567ce13deed120e86
htbmixbox <mizdebsk@gmail.com>
2024-04-25 02:56:45 -0700

    1.3

--- CSLab/Labs/1.3/P1_3.cs
+++ CSLab/Labs/1.3/P1_3.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,20 @@ internal class P1_3
 
     public void main(String[] args)
     {
-        //Napisz w edytorze tekstowym program w C# wypisujący tekst podany jako parametr uruchomienia. Skompiluj z linii komend i uruchom.
-        Console.WriteLine("Program 1.1:\nEnter text:");
-        string intput = Console.ReadLine();
-        Console.WriteLine("Entered: "+intput);
+            //Napisz program, który od podanej ceny netto i stawki VAT oblicza kwotę VAT i cenę brutto.
+            //Np. cena netto 100 zł, stawka 22% daje kwotę VAT 22 zł i cenę brutto 122 zł. 
+            //Wyświetlaj kwoty z dwoma miejscami po przecinku, wyświetlając dodawaj symbol waluty i znak  % (procent).
+            Console.WriteLine("Program 1.3:\nIle zarabiasz netto na F/V?:");
+                try { 
+                    decimal netto = decimal.Parse(Console.ReadLine());
+                    Console.WriteLine("Ile % F/Vat?:");
+                    decimal vat = decimal.Parse(Console.ReadLine());
+                    decimal kwota = Math.Round(netto + netto * (vat/100), 2);
+                    Console.WriteLine("Kwota brutton:"+kwota);
+            }catch (FormatException ex) {
+                Console.WriteLine("Wrong data!");
+                this.main(args);
+            }
+        }
     }
-}
 }
