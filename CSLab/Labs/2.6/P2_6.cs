abdbb13ef6c3165fe47662371c8023fa704c06fc
htbmixbox <mizdebsk@gmail.com>
2024-04-26 15:25:25 -0700

    2.6

--- CSLab/Labs/2.6/P2_6.cs
+++ CSLab/Labs/2.6/P2_6.cs
@@ -1,20 +1,36 @@
-﻿using System;
+﻿using CSLab.Labs._2._6;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+ * 
+II.6 (nowy projekt ‘produkt’)
+Utwórz klasę o nazwie Produkt.
+W klasie zdefiniuj pola reprezentujące nazwę, cenę zakupu, jednostkę miary, datę zakupu, stawkę VAT, marżę (zysku). 
+Zaimplementuj konstruktor klasy (przekazujący wartości do pól) oraz metodę wypisującą dane produktu na konsolę.
+Zaimplementuj konstruktor tworzący obiekt w oparciu o wartości podane z konsoli.
+Zaimplementuj metodę Clone() tworzącą kopię obiektu.
+Przetestuj zaimplementowane składowe pisząc odpowiedni kod testowy w metodzie Main().
 
+ */
 namespace CSLab
 {
     internal class P2_6
     {
         public void main(string[] args)
         {
-          
+            Produkt produkt = new Produkt("cukier", 9.4f, "kg", DateTime.Now, 23.0f);
+            Console.WriteLine(produkt);
+            produkt.printData();
+            Console.WriteLine(produkt.clone);
+            Debug.Assert(produkt != produkt.clone());
 
-   
-        
+
+
         }
     }
 }
