719f8fe212330e0bb76df514151a9961cb6af21e
htbmixbox <mizdebsk@gmail.com>
2024-04-27 11:54:42 -0700

    2.11

--- CSLab/Labs/2.11/P2_11.cs
+++ CSLab/Labs/2.11/P2_11.cs
@@ -1,15 +1,34 @@
-﻿using System;
+﻿using CSLab.Labs._2._11;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CSLab
 {
     internal class P2_11
+/*Utwórz klasę RandomUtility zawierającą narzędzia do generowania losowego.
+W klasie zdefiniuj metody: 
+a. do losowego generowania liczby z podanego przedziału dla różnych typów liczbowych(int, decimal, double): randomInt(a, b), randomDecimal(a, b), randomDouble(a, b)
+b. do losowego generowania łańcucha znakowego(stringa) składającego się z n dowolnych znaków: randomString(n)
+dla uproszczenia można założyć, że string ma składać się wyłącznie z liter
+c. do losowego wybierania n elementów z podanej tablicy (z możliwością powtórzeń lub bez): randomFromArray(n, array, rep)
+Przetestuj te metody w klasie testowej(z metodą Main()).
+*/
     {
         public void main(string[] args)
         {
+            Console.WriteLine("Random double:");
+            Console.WriteLine(RandomUtility.randomDouble(1.0, 3.0));
+            Console.WriteLine("Random string:");
+            Console.WriteLine(RandomUtility.randomString(11));
+            Console.WriteLine("Random int:");
+            Console.WriteLine(RandomUtility.randomInt(1,100));
+
+
            
         }
     }
---
+++ CSLab/Labs/2.11/RandomUtility.cs
@@ -0,0 +1,72 @@
+﻿/*Utwórz klasę RandomUtility zawierającą narzędzia do generowania losowego.
+W klasie zdefiniuj metody: 
+a. do losowego generowania liczby z podanego przedziału dla różnych typów liczbowych(int, decimal, double): randomInt(a, b), randomDecimal(a, b), randomDouble(a, b)
+b. do losowego generowania łańcucha znakowego(stringa) składającego się z n dowolnych znaków: randomString(n)
+dla uproszczenia można założyć, że string ma składać się wyłącznie z liter
+c. do losowego wybierania n elementów z podanej tablicy (z możliwością powtórzeń lub bez): randomFromArray(n, array, rep)
+Przetestuj te metody w klasie testowej(z metodą Main()).
+*/
+using System;
+using System.Dynamic;
+
+namespace CSLab.Labs._2._11
+{
+    class RandomUtility
+    {
+        private static readonly Random random = new Random();
+        public static int randomInt(int start, int end) {
+            return random.Next(start, end);
+        }
+        public static decimal randomDecimal(decimal start, decimal end) {
+            return start + (decimal)(random.NextDouble() * (double)(end - start));
+        }
+        public static double randomDouble(double start, double end) {
+            return start + (random.NextDouble() * (end - start));
+        }
+        public static string randomString(int length) {
+            string charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            char[] result = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = charSet[random.Next(charSet.Length)];
+            }
+            return new string(result);
+        }
+
+        static object[] RemoveElementFromArray(object[] array, int index)
+        {
+            if (index >= 0 && index < array.Length)
+            {
+                List<object> tempList = new List<object>(array);
+                tempList.RemoveAt(index);
+                return tempList.ToArray();
+            }
+            else
+            {
+                Console.WriteLine("Invalid index. Index is out of range.");
+                return array;
+            }
+        }
+
+        public static object[] randomFromArray(int n, object[] array, bool repeat) {
+            if (!repeat&&array.Length<n) {
+                throw new Exception("taki przypadek nie jest mozliwy");
+            }
+
+            object[] objects = new object[n];
+            
+                for (int i = 0; i < n; i++) {
+                    int randInt = randomInt(0, array.Length);
+                    objects[i] = array[randInt];
+                    if (!repeat)
+                        {
+                          array = RemoveElementFromArray(array, randInt);
+    
+                      }
+                }
+            return objects;
+
+        }
+
+    }
+}
