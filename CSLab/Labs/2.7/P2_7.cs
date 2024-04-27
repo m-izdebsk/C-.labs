ad0a554e9e439aa1f5a99213012e819d722f75b8
htbmixbox <mizdebsk@gmail.com>
2024-04-27 01:57:41 -0700

    2.7

--- CSLab/Labs/2.7/P2_7.cs
+++ CSLab/Labs/2.7/P2_7.cs
@@ -9,11 +9,20 @@ namespace CSLab
 {
     internal class P2_7
     {
+        //Przetestuj zaimplementowane składowe pisząc odpowiedni kod testowy w metodzie Main().
+
         public void main(string[] args)
         {
-            Sprzedaz sprzedaz = new Sprzedaz("test", 1, 12);
-            sprzedaz.printData();
-
+            
+            var s1 = new Sprzedaz("Auto", 2, new(13123.25));
+            var s2 = new Sprzedaz("Cukier", 2, new(10.25));
+            var s3 = new Sprzedaz("Ryba", 2, new(2.25));
+            var z = new Zamowienie(DateTime.Now, "Google");
+            z.DodajPozycje(s1);
+            z.DodajPozycje(s2);
+            z.DodajPozycje(s3);
+            s1.printData();
+            z.info();
         }
     }
 }
--- CSLab/Labs/2.7/Zamowienie.cs
+++ CSLab/Labs/2.7/Zamowienie.cs
@@ -1,22 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /*
  
 Utwórz klasę Zamowienie. 
-W klasie zdefiniuj pola reprezentujące datę realizacji, nazwę klienta, pozycje zamównienia (jako tablicę obiektów klasy Sprzedaz, zakładając obecność maksymalnie 20-stu pozycji w jednym zamówieniu).
+W klasie zdefiniuj pola reprezentujące datę realizacji, nazwę klienta, pozycje zamównienia (jako tablicę obiektów klasy Sprzedaz, 
+zakładając obecność maksymalnie 20-stu pozycji w jednym zamówieniu).
 Zaimplementuj konstruktor, który przekazuje datę i kupującego oraz przydziela pamięć dla tablicy pozycji zamówienia.
 Zaimplementuj metodę dodajPozycje() dodającą do zamówienia kolejną pozycję. Zaimplementuj dwie postaci tej metody:
 dodajPozycje(Sprzedaz pozycja) oraz dodajPozycje(string p, int a, decimal sP).
 Zaimplementuj metodę wypisującą informacje o zamówieniu (w tym informacje o wszystkich pozycjach tego zamówienia) na konsoli.
-Przetestuj zaimplementowane składowe pisząc odpowiedni kod testowy w metodzie Main().
 
 */
 namespace CSLab.Labs._2._7
 {
-    internal class Zamowienie
+    public  class Zamowienie
     {
+        DateTime dataRealizacji;
+        string nazwaKlienta;
+        Sprzedaz[] pozycjeZamowienia;
+        private int _counter = 0;
+
+        public Zamowienie(DateTime data, string klient)
+        {
+            dataRealizacji = data;
+            nazwaKlienta = klient;
+            pozycjeZamowienia = new Sprzedaz[20];
+        }
+        public void DodajPozycje(Sprzedaz pozycja)
+        {
+            if (_counter == 20)
+            {
+                Console.WriteLine("Nie można dodać więcej pozycji!");
+                return;
+            }
+            pozycjeZamowienia[_counter++] = pozycja;
+        }
+
+        public void DodajPozycje(string p, int a, decimal sP)
+        {
+            if (_counter == 20)
+            {
+                Console.WriteLine("Nie można dodać więcej pozycji!");
+                return;
+            }
+            pozycjeZamowienia[_counter++] = new Sprzedaz(p, a, sP);
+        }
+        public void info()
+        {
+            Console.WriteLine($"Data realizacji: {this.dataRealizacji}");
+            Console.WriteLine($"Nazwa klienta: {this.nazwaKlienta}");
+            
+            for (int i = 0; i< _counter; i++) {
+                Console.WriteLine();
+                Console.WriteLine($"Pozycja nr {(i+1)}");
+                pozycjeZamowienia[i].printData();
+                Console.WriteLine();
+            }
+           
+        }
+
     }
 }
