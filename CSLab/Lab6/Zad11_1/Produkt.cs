20cad9d4ab9e7725c0e731dcd6cc1dfa453c8f0a
htbmixbox <mizdebsk@gmail.com>
2024-05-26 08:07:08 -0700

    11.1

---
+++ CSLab/Lab6/Zad11_1/Produkt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLab.Lab6.Zad11_1
+{
+    internal class Produkt
+    {
+        string nazwa;
+        double cena;
+        double vat;
+        public Produkt(string nazwa, double cena, double vat)
+        {
+            this.nazwa = nazwa;
+            this.cena = cena;
+            this.vat = vat;
+        }
+        public void setVat(double vat) {
+            this.vat = vat;
+        }
+        public override string ToString()
+        {
+            return "Nazwa:" + this.nazwa + "\n" + "vat:" + this.vat;
+        }
+    }
+}
---
+++ CSLab/Lab6/Zad11_1/Zad11_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLab.Lab6.Zad11_1
+{
+    internal class Zad11_1
+    {
+        public void main(string[] args) {
+            Produkt produkt = new Produkt("test", 1.0d, 1.0d);
+            Console.WriteLine("Podaj cene:");
+            try
+            {
+                double cena = Double.Parse(Console.ReadLine());
+                produkt.setVat(cena);
+            }
+            catch (System.FormatException ex)
+            {
+                Console.WriteLine("Cos poszlo nie tak");
+            }
+
+            Console.WriteLine(produkt.ToString());
+
+
+        }
+    }
+}
