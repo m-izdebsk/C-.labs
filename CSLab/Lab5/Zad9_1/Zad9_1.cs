bd418a95b051b96bc7aff6422cc1afa657686000
htbmixbox <mizdebsk@gmail.com>
2024-05-26 07:23:19 -0700

    clenaup

--- CSLab/Lab5/Zad9_1/Zad9_1.cs
+++ CSLab/Lab5/Zad9_1/Zad9_1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace CSLab.Lab5.Zad9_1
 {
     internal class Zad9_1
