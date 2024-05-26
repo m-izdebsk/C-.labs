6809dc863c9823c1595be834d492e2991689338b
htbmixbox <mizdebsk@gmail.com>
2024-05-26 07:41:13 -0700

    9.3 update

--- CSLab/Lab5/Zad9_3/Zad9_3.cs
+++ CSLab/Lab5/Zad9_3/Zad9_3.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSLab.Lab5.Zad9_3
+﻿namespace CSLab.Lab5.Zad9_3
 {
     internal class Zad9_3
     {
-        public void main(string[] args) {
-
+        public void main(string[] args)
+        {
             Grupa grupa = new Grupa(Grupa.wypisz3);
             grupa.Wypisz();
             grupa.zmienWypisywania(Grupa.wypisz2);
@@ -17,7 +11,5 @@ namespace CSLab.Lab5.Zad9_3
             grupa.zmienWypisywania(Grupa.wypisz1);
             grupa.Wypisz();
         }
-        
-
     }
 }
