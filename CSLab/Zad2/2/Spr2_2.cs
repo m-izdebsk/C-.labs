dbdf43387e25a0425771870a6a6fd1e3d8d56955
htbmixbox <mizdebsk@gmail.com>
2024-06-01 13:44:55 -0700

    9.2

---
+++ CSLab/Zad2/2/Spr2_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLab.Zad2._2
+{
+    internal class Spr2_2
+    {
+
+    }
+}
---
+++ CSLab/Zad2/2/Zad9_2.cs
@@ -0,0 +1,44 @@
+﻿
+//protected delegate decimal Działanie(decimal x, decimal y);
+
+
+namespace CSLab.Zad2._2;
+
+
+internal class Zad9_2
+{
+    decimal Dodaj(decimal x, decimal y) => x + y;
+    decimal Odejmij(decimal x, decimal y) => x - y;
+    decimal Pomnóż(decimal x, decimal y) => x * y;
+    decimal Podziel(decimal x, decimal y) => y != 0 ? x / y : 0;
+
+    public void main(string[] args)
+    {
+        Console.WriteLine("Podaj trzy liczby:");
+
+        // Pobieranie trzech liczb z klawiatury
+        decimal liczba1 = Convert.ToDecimal(Console.ReadLine());
+        decimal liczba2 = Convert.ToDecimal(Console.ReadLine());
+        decimal wynik = Convert.ToDecimal(Console.ReadLine());
+
+        // Tablica z dostępnymi operacjami
+        Działanie[] operacje = { Dodaj, Odejmij, Pomnóż, Podziel };
+
+        // Przejście przez wszystkie operacje i znalezienie odpowiedniej
+        Działanie najlepszeDziałanie = null;
+        decimal najlepszyWynik = decimal.MaxValue;
+        foreach (Działanie operacja in operacje)
+        {
+            decimal obliczonyWynik = operacja(liczba1, liczba2);
+            decimal różnica = Math.Abs(obliczonyWynik - wynik);
+            if (różnica < najlepszyWynik)
+            {
+                najlepszyWynik = różnica;
+                najlepszeDziałanie = operacja;
+            }
+        }
+
+        Console.WriteLine($"Pierwsze dziaanie spelnajace wymagania: {najlepszeDziałanie.Method.Name}({liczba1}, {liczba2}) = {najlepszeDziałanie(liczba1, liczba2)}");
+    }
+
+}
